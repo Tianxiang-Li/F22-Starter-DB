f6d72de3830743d09733cab41f4348f7e342782f
TXL-Matthew <tl3091@columbia.edu>
2022-10-07 03:56:25 -0400

    modified

--- some_columbia_student_sql.sql
+++ some_columbia_student_sql.sql
@@ -16,10 +16,4 @@ create table columbia_student
         unique (uni)
 );
 
-/*
- Example insert statement. Note that since auto_id is auto-increment, the insert
- statement does not need to specify it.
- */
- insert into columbia_student (last_name, first_name, middle_name, email, uni)
-    values ("Ferguson", "Donald", "Francis", "dff@cs.columbia.edu", "dff9");
 
